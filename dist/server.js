64ac895e8df2e92a084b109ea43ac0ec33ed69df
bacas07 <mateobacas07@gmail.com>
2025-04-20 17:10:46 -0500

    feat: Archivo server creado para montar el servidor

--- dist/server.js
+++ dist/server.js
@@ -1,1 +1,12 @@
-"use strict";
+import express from 'express';
+const PORT = 3000;
+const server = express();
+server.use(express.json());
+try {
+    server.listen(PORT, () => {
+        console.log('Servidor activo en el puerto: ', PORT);
+    });
+}
+catch (error) {
+    console.log('Error activando el servidor');
+}
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const PORT = 3000;
+const server = express();
+
+server.use(express.json());
+
+try {
+  server.listen(PORT, () => {
+    console.log('Servidor activo en el puerto: ', PORT);
+  })
+} catch (error) {
+  console.log('Error activando el servidor')
+}
