b79ad7420b7378298def14d3f53bd0fc6cd54d74
bacas07 <mateobacas07@gmail.com>
2025-04-21 08:14:36 -0500

    feat: Instalacion y configuracion de dotenv para la configuracion de variables de entorno

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { config } from 'dotenv';
+
+config();
 
 const PORT = 3000;
 const server = express();
@@ -8,7 +11,7 @@ server.use(express.json());
 try {
   server.listen(PORT, () => {
     console.log('Servidor activo en el puerto: ', PORT);
-  })
+  });
 } catch (error) {
-  console.log('Error activando el servidor')
+  console.log('Error activando el servidor');
 }
