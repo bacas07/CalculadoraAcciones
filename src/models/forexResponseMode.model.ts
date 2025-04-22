84f7c532f01658ae8fbd7649dfc72b87da31a5cd
Mateo Baca <mateobacas07@gmail.com>
2025-04-22 08:44:20 -0500

    feat: metodo para ver todos los ForexResponse creados

--- src/models/forexResponseMode.model.ts
+++ src/models/forexResponseMode.model.ts
@@ -1,4 +1,11 @@
 import { ForexResponseModel } from '../schemas/forexResponse.schema.js';
 import type { ForexResponse } from '../types/types.js';
 
-export default class ForexResponseServive {}
+export default class ForexResponseServive {
+  findAll = async () => {
+    try {
+    } catch (error) {
+      return console.error('Error ForexResponseModel findAll: ', error);
+    }
+  };
+}
