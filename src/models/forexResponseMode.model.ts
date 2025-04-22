5e7c99dc83fbd614d03b4d3e05828f48d5fefe3d
Mateo Baca <mateobacas07@gmail.com>
2025-04-22 08:47:18 -0500

    feat: metodo para buscar un ForexResponser por su ID

--- src/models/forexResponseMode.model.ts
+++ src/models/forexResponseMode.model.ts
@@ -4,8 +4,17 @@ import type { ForexResponse } from '../types/types.js';
 export default class ForexResponseServive {
   findAll = async () => {
     try {
+      return await ForexResponseModel.find();
     } catch (error) {
       return console.error('Error ForexResponseModel findAll: ', error);
+    }
+  };
+
+  findByID = async (id: string) => {
+    try {
+      return ForexResponseModel.findById(id);
+    } catch (error) {
+      return console.error('Error ForexResponseModel findByID: ', error);
     }
   };
 }
