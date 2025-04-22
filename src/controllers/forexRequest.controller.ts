2482bf13119584baaa2bb79a69ee93aecbc2ed8d
Mateo Baca <mateobacas07@gmail.com>
2025-04-22 11:57:15 -0500

    feat: Funcion para obtener todos los forex requests agregada a forexRequestController.ts

--- src/controllers/forexRequest.controller.ts
+++ src/controllers/forexRequest.controller.ts
@@ -3,7 +3,18 @@ import ForexRequestService from '../models/forexRequest.model.js';
 
 export const findAllForexRequest = async (req: Request, res: Response) => {
   try {
-    const requests = await ForexRequestService;
+    const requests = await ForexRequestService.findAll();
+
+    if (!requests) {
+      return res.status(200).json({ message: 'No forex request found' });
+    }
+
+    return res
+      .status(200)
+      .json({
+        message: `${requests.length} forex request founds`,
+        requests: requests,
+      });
   } catch (error) {
     console.error('Error forexRequestController findAll');
     return res.status(500).json({ error: 'Error creating forex request' });
