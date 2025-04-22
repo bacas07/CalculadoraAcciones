05efe1b9a0156642fc176c4356ff740a81994d10
Mateo Baca <mateobacas07@gmail.com>
2025-04-22 12:10:24 -0500

    feat: Funcion para obtener un request por su id forexRequestController.ts

--- src/controllers/forexRequest.controller.ts
+++ src/controllers/forexRequest.controller.ts
@@ -9,14 +9,32 @@ export const findAllForexRequest = async (req: Request, res: Response) => {
       return res.status(200).json({ message: 'No forex request found' });
     }
 
-    return res
-      .status(200)
-      .json({
-        message: `${requests.length} forex request founds`,
-        requests: requests,
-      });
+    return res.status(200).json({
+      message: `${requests.length} forex request founds`,
+      requests: requests,
+    });
   } catch (error) {
     console.error('Error forexRequestController findAll');
-    return res.status(500).json({ error: 'Error creating forex request' });
+    return res.status(500).json({ error: 'Error finding all forex request' });
+  }
+};
+
+export const findByIDForexRequest = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const request = await ForexRequestService.findByID(id);
+
+    if (!request) {
+      return res
+        .status(404)
+        .json({ message: `No request forex found with id: ${id}` });
+    }
+
+    return res
+      .status(200)
+      .json({ message: `forex request foud with id: ${id}`, request: request });
+  } catch (error) {
+    console.error('Error forexRequestController findByID');
+    return res.status(500).json({ error: 'Error finding forex request by id' });
   }
 };
