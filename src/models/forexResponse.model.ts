56999a23a6ff6e361ebf91ae39a8c47dd853c748
Mateo Baca <mateobacas07@gmail.com>
2025-04-22 11:45:04 -0500

    feat: Cambiando la declaracion de metodos dentre de las clases de los modelos

--- src/models/forexResponse.model.ts
+++ src/models/forexResponse.model.ts
@@ -1,45 +1,47 @@
 import { ForexResponseModel } from '../schemas/forexResponse.schema.js';
 import type { IForexResponse } from '../types/types.js';
 
-export default class ForexResponseServive {
-  findAll = async () => {
+class ForexResponseServive {
+  async findAll() {
     try {
       return await ForexResponseModel.find();
     } catch (error) {
       return console.error('Error ForexResponseModel findAll: ', error);
     }
-  };
+  }
 
-  findByID = async (id: string) => {
+  async findByID(id: string) {
     try {
       return ForexResponseModel.findById(id);
     } catch (error) {
       return console.error('Error ForexResponseModel findByID: ', error);
     }
-  };
+  }
 
-  findByRequestID = async (requestID: string) => {
+  async findByRequestID(requestID: string) {
     try {
       return ForexResponseModel.find({ requestID });
     } catch (error) {
       return console.error('Error ForexResponseModel findByRequestID: ', error);
     }
-  };
+  }
 
-  create = async (response: IForexResponse) => {
+  async create(response: IForexResponse) {
     try {
       const created = await ForexResponseModel.create(response);
       return created;
     } catch (error) {
       return console.error('Error ForexResponseModel create: ', error);
     }
-  };
+  }
 
-  delete = async (id: string) => {
+  async delete(id: string) {
     try {
       return await ForexResponseModel.findByIdAndDelete(id);
     } catch (error) {
       return console.error('Error ForexResponseModel delete: ', error);
     }
-  };
+  }
 }
+
+export default new ForexResponseServive();
