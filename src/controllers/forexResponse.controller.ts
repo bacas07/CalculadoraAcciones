f0972c147becf8a2faeb100d46da814e733a9429
Mateo Baca <mateobacas07@gmail.com>
2025-04-22 21:08:11 -0500

    feat: Funcion para obtener un response por su request id asignado forexResponseController.ts

--- src/controllers/forexResponse.controller.ts
+++ src/controllers/forexResponse.controller.ts
@@ -30,14 +30,36 @@ export const findByIDForexResponse = async (req: Request, res: Response) => {
         .json({ message: `No forex response found with id: ${id}` });
     }
 
-    return res
-      .status(200)
-      .json({
-        message: `forex response found with id: ${id}`,
-        response: response,
-      });
+    return res.status(200).json({
+      message: `forex response found with id: ${id}`,
+      response: response,
+    });
   } catch (error) {
     console.error('Error forexResponseController findByID: ', error);
     return res.status(500).json({ error: 'Error findind response with id' });
+  }
+};
+
+export const findByRequestIDForexResponse = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+    const responses = await ForexResponseService.findByRequestID(id);
+
+    if (!responses) {
+      return res
+        .status(404)
+        .json({ message: `No forext responses found with Request id: ${id}` });
+    }
+
+    return res.status(200).json({
+      message: `${responses.length} forex responses found with request id: ${id}`,
+      responses: responses,
+    });
+  } catch (error) {
+    console.error('Error forexResponseController findByRequestID: ', error);
+    return res.status(500);
   }
 };
