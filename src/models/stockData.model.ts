d19c9c51ea6af4dcf971697a899582b5588196f0
Mateo Baca <mateobacas07@gmail.com>
2025-04-30 20:09:20 -0500

    feat: Modelo stockData agregado para operar sobre base de datos en stockData.model.ts

--- src/models/stockData.model.ts
+++ src/models/stockData.model.ts
@@ -2,11 +2,40 @@ import { StockDataModel } from '../schemas/stockData.schema.js';
 import type { IStockDataPoint } from '../types/types.js';
 
 export class StockDataService {
-  async findAll() {
+  async getAll(): Promise<IStockDataPoint[] | null> {
     try {
       return await StockDataModel.find();
     } catch (error) {
-      return console.error('Error StockDataModel findAll: ', error);
+      console.error('Error StockDataModel findAll: ', error);
+      throw new Error('Error StockDataModel findAll');
+    }
+  }
+
+  async getOneByID(id: string): Promise<IStockDataPoint | null> {
+    try {
+      return await StockDataModel.findById(id);
+    } catch (error) {
+      console.error('Error StockDataModel getOne: ', error);
+      throw new Error('Error StockDataModel getOne');
+    }
+  }
+
+  async create(data: IStockDataPoint): Promise<IStockDataPoint | null> {
+    try {
+      const stockCreated = await StockDataModel.create(data);
+      return stockCreated;
+    } catch (error) {
+      console.error('Error StockDataModel create: ', error);
+      throw new Error('Error StockDataModel create');
+    }
+  }
+
+  async delete(id: string): Promise<any | null> {
+    try {
+      return await StockDataModel.findByIdAndDelete(id);
+    } catch (error) {
+      console.error('Error StockDataModel delete: ', error);
+      throw new Error('Error StockDataModel delete');
     }
   }
 }
