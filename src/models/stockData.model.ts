fca188ae3b4b6545d6976d4c75d2bb6287967926
Mateo Baca <mateobacas07@gmail.com>
2025-04-30 19:16:38 -0500

    feat: Funcion para ver todos los registros de Stock en stockData.model.ts

--- src/models/stockData.model.ts
+++ src/models/stockData.model.ts
@@ -1,4 +1,12 @@
 import { StockDataModel } from '../schemas/stockData.schema.js';
 import type { IStockDataPoint } from '../types/types.js';
 
-export class StockDataService {}
+export class StockDataService {
+  async findAll() {
+    try {
+      return await StockDataModel.find();
+    } catch (error) {
+      return console.error('Error StockDataModel findAll: ', error);
+    }
+  }
+}
