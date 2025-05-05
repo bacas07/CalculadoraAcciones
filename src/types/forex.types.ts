16c71c9d5a31301a4c24350397ab8fcb15569aad
Mateo Baca <mateobacas07@gmail.com>
2025-05-05 15:56:25 -0500

    chore: Creacion de archivos para types para intercambio de divisas forex.types.ts

---
+++ src/types/forex.types.ts
@@ -0,0 +1,31 @@
+import { Document } from "mongoose";
+
+// Euro vs Dolar estadounidense schema especial de mongoose
+export interface IeurUsdMongoose extends Document {
+
+}
+
+// Euro vs Dolar estadounidense
+export interface IeurUsd {
+
+}
+
+// Dolar estadounidense vs Yen japones schema especial de mongoose
+export interface IusdJpyMongoose extends Document {
+
+}
+
+// Dolar estadounidense vs Yen japones
+export interface IusdJpy {
+
+}
+
+// Libra esterlina vs Dolar estadounidense schema especial de mongoose
+export interface IgbpUsdMongoose extends Document {
+
+}
+
+// Libra esterlina vs Dola estadounidense
+export interface IgbpUsd {
+
+}
