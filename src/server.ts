2c5c41ca2e40fa8f53e2628fca99ab5e048acf4b
Mateo Baca <mateobacas07@gmail.com>
2025-05-12 11:52:02 -0500

    chore: Eliminando archivos obsoletos de versiones antereiores

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import express, { Request, Response } from 'express';
 import { connectDB } from './database/mongo.js';
-import requestRouter from './routes/forexRequest.routes.js';
-import responseRouter from './routes/forexResponse.routes.js';
 import EurUsdRouter from './routes/eurUsd.routes.js';
 import GbpUsdRouter from './routes/gbpUsd.routes.js';
 import UsdJpyRouter from './routes/UsdJpy.routes.js';
@@ -38,10 +36,6 @@ server.use(morgan);
 server.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Welcome to Forex Predict' });
 });
-
-// Rutas antiguas sin funcionalidad en el nuevo codigo
-server.use('/forex-request', requestRouter);
-server.use('/forex-response', responseRouter);
 
 // Rutas nuevas para predicciones reales
 server.use('/eurusd', EurUsdRouter);
